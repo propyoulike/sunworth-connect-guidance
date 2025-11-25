fd63fd295497e02f3d72f510f4d959d6daba3243
PropYouLike <propyoulike@gmail.com>
2025-11-25 11:01:45 +0530

    Update Location.tsx

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -205,7 +205,9 @@ const Location = ({ onCtaClick }: LocationProps) => {
                 variant="outline"
                 className="text-lg px-8 py-6 rounded-full font-semibold"
               >
+              <a id="whatsapp-btn" href="https://wa.me/919379822010" target="_blank">
                 Chat on WhatsApp
+              </a>
               </Button>
             </a>
           </div>
