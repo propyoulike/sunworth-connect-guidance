fd0c9a30752a1a1c17498bcaa14e24c34e452b2e
PropYouLike <propyoulike@gmail.com>
2025-11-26 08:35:35 +0530

    Update Location.tsx

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -81,7 +81,7 @@ export default function Location({ onCtaClick }: LocationProps) {
           >
             {mapVisible && (
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.430845743881!2d77.485!3d12.936!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTLCsDU2JzA5LjYiTiA3N8KwMjknMDYuMCJF!5e0!3m2!1sen!2sin!4v1700000000000"
+                src="https://www.google.com/maps/d/embed?mid=1R_qhSGztiUmFoQY8idXKpXjU-kF60pQ&ehbc=2E312F"
                 className="w-full h-full rounded-xl shadow-lg"
                 loading="lazy"
                 title="Location Map"
