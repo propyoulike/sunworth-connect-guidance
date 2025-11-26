c3258af43677802eaf034363c6d6c9ee34d79300
PropYouLike <propyoulike@gmail.com>
2025-11-26 07:44:00 +0530

    Update CustomerSpeaks.tsx

--- src/components/CustomerSpeaks.tsx
+++ src/components/CustomerSpeaks.tsx
@@ -100,7 +100,7 @@ const CustomerSpeaks = ({ onCtaClick }: CustomerSpeaksProps) => {
             className="btn-gradient text-lg px-8 py-6 rounded-full font-semibold"
             onClick={onCtaClick}
           >
-            Get Personalised Guidance with FREE Site Visit
+            Book FREE Site Visit
           </Button>
         </div>
       </div>
