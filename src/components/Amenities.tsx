3795a8299e564f61e016719aefb4b2cd1320b3a8
PropYouLike <propyoulike@gmail.com>
2025-11-26 07:42:05 +0530

    Update Amenities.tsx

--- src/components/Amenities.tsx
+++ src/components/Amenities.tsx
@@ -122,7 +122,7 @@ const Amenities = ({ onCtaClick }: AmenitiesProps) => {
             className="btn-gradient text-lg px-8 py-6 rounded-full font-semibold"
             onClick={onCtaClick}
           >
-            Get Personalised Guidance with FREE Site Visit
+            Book FREE Site Visit
           </Button>
         </div>
       </div>
